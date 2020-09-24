391c3fd8d2e114f9e2bb7605aaf93b3cf7d1594f
Noel Nunkovich <noel.nunkovich@veolia.com>
2020-09-24 14:03:35 -0400

    Fix problems with venv creation

--- install.sh
+++ install.sh
@@ -1,71 +1,73 @@
-#!/bin/sh
+#!/bin/bash
 
 # Set .inputrc to enable Vi command mode on terminal
+echo "Setting Vi command mode in terminal"
 ln -s $DIR/inputrc ~/.inputrc
 
 # Get the installation directory
 DIR=$(pwd)
 
-# Create the symlink for .vmrc
-ln -s $DIR/vimrc ~/.vimrc
-
 # Create symlink for TMux Config
+echo "Installing TMUX configuration"
 ln -s $DIR/tmux.conf ~/.tmux.conf
 
 # Create symlink for tmuxp
 ln -s $DIR/tmuxp/ ~/.tmuxp
 
-# Make sure .vim directory exists before we
-# try to create a symlink in it.
-if [ ! -d ~/.vim ]; then
-    mkdir ~/.vim
-fi
-
-# .vim directory exists. If there's already a 
-# colors/ directory in it, move it so we can
-# create a symlink.
-if [ -d ~/.vim/colors ]; then
-    echo "A .vim/colors directory exists. Moving to .vim/colors.old"
-    mv ~/.vim/colors.old
-fi
-
-# Create symlink for colors folder
-ln -s $DIR/vim-colors/ ~/.vim/colors
-
-# Download and install Vundle
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# Run Vim to install Plugins
-vim +PluginInstall +qall
-
 # Install Neovim
-sudo add-apt-repository ppa:neovim-ppa/stable
-sudo apt update
-sudo apt install -y ctags vim-scripts neovim 
+#sudo add-apt-repository ppa:neovim-ppa/stable
+#sudo apt update
+if hash neovim 2>/dev/null; then
+    echo "Installing Neovim"
+    sudo apt install -y neovim 
+else
+    echo "Neovim found"
+fi
 
 if hash virtualenv 2>/dev/null; then
+    echo "Installing Python virtualenv"
     sudo apt install -y virtualenv
+else
+    echo "Python virtualenv found"
 fi
 
 cd ~/.config/nvim
-virtualenv -p python2 env2
-source ~/.config/nvim/env2/bin/activate
-pip install neovim
-deactivate
-
-virtualenv -p python3 env3
-source ~/.config/nvim/env3/activate
-pip install neovim
-deactivate
-
-mkdir ~/.config/nvim
-ln -s $DIR/nvim/init.vim ~/.config/nvim/init.vim
-
-if hash curl 2>/dev/null; then
-    sudo apt install -y curl
+if [ ! -d ~/.config/nvim/env2 ]; then
+    echo "Setting up Python 2 virtual environment for Neovim"
+    python2 -m virtualenv env2
+    source ~/.config/nvim/env2/bin/activate
+    pip install pynvim
+    deactivate
 fi
 
+if [ ! -d ~/.config/nvim/env3 ]; then
+    echo "Setting up Python 3 virtual environment for Neovim"
+    python3 -m venv env3
+    source ~/.config/nvim/env3/bin/activate
+    pip install pynvim
+    deactivate
+fi
+
+echo "Installing Neovim configurations"
+if [ ! -d ~/.config/nvim ]; then
+    mkdir ~/.config/nvim
+else
+    mkdir ~/.config/nvim.bak
+    mv ~/.config/nvim/* ~/.config/nvim.bak
+fi
+ln -s $DIR/nvim/init.vim ~/.config/nvim/init.vim
+ln -s $DIR/nvim/configs/ ~/.config/nvim/configs
+
+if hash curl 2>/dev/null; then
+    echo "Installing Curl"
+    sudo apt install -y curl
+else
+    echo "Curl found"
+fi
+
+echo "Installing Plugged plugin for Neovim"
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
     https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-nvim +PluginInstall +UpdateRemotePlugins +qa
+echo "Running Neovim to install plugins"
+nvim +PlugInstall +UpdateRemotePlugins +qa
