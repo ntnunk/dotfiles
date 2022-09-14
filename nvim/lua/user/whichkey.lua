fa9a58e959b40f7ec8f51fea4024e621f95b8d93
Noel Nunkovich <nnunkovich@fortressinfosec.com>
2022-09-14 12:07:10 -0400

    Fortress: add missing <cr> to mappings

--- nvim/lua/user/whichkey.lua
+++ nvim/lua/user/whichkey.lua
@@ -161,10 +161,10 @@ local mappings = {
   },
   o = {
     name = "Harpoon",
-    m = { "<cmd>lua require'harpoon.mark'.add_file()", "Add file" },
-    q = { "<cmd>lua require'harpoon.ui'.toggle_quick_menu()", "Menu" },
-    n = { "<cmd>lua require'harpoon.ui'.nav_next()", "Next file" },
-    p = { "<cmd>lua require'harpoon.ui'.nav_prev()", "Previous file" },
+    m = { "<cmd>lua require'harpoon.mark'.add_file()<cr>", "Add file" },
+    q = { "<cmd>lua require'harpoon.ui'.toggle_quick_menu()<cr>", "Menu" },
+    n = { "<cmd>lua require'harpoon.ui'.nav_next()<cr>", "Next file" },
+    p = { "<cmd>lua require'harpoon.ui'.nav_prev()<cr>", "Previous file" },
   },
   s = {
     name = "Search",
