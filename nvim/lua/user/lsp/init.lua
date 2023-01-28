5a6e8114de2d6f4f8b5746b6117c6f857d6298bd
Noel <ntnunk@gmail.com>
2023-01-28 12:22:30 -0500

    updates to fedora workstation

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -6,3 +6,22 @@ end
 require "user.lsp.lsp-installer"
 require("user.lsp.handlers").setup()
 require "user.lsp.null-ls"
+
+require("mason").setup()
+require("mason-lspconfig").setup()
+require("mason-lspconfig").setup_handlers {
+  -- the first entry (without a key) will be the default handler
+  -- and will be called for each installed server that doesn't have
+  -- a dedicated handler
+  function(server_name) -- default handler (optional)
+    require("lspconfig")[server_name].setup {}
+  end,
+
+  -- Next, you can provide a dedicated handler for specific servers.
+  -- For example, a handler override for assembler server to add file type
+  ["asm_lsp"] = function()
+    require("lspconfig")["asm_lsp"].setup {
+      filetypes = {"asm", "s", "S", "vasm"},
+    }
+  end
+}
