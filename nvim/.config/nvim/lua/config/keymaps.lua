40c22e26ed7585d1e20c44eead5238df7691e9f1
Noel Nunkovich <ntnunk@gmail.com>
2025-02-16 10:50:28 -0500

    Add custom lazy.nvim config

---
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -0,0 +1,57 @@
+local keymap = vim.api.nvim_set_keymap
+local opts = { noremap = true, silent = true }
+
+--Remap space as leader key
+keymap("", "<Space>", "<Nop>", opts)
+vim.g.mapleader = " "
+--vim.g.maplocalleader = ","
+
+-- Disable EX mode since I'm always hitting Q by mistake
+keymap("", "Q", "<Nop>", opts)
+
+-- Modes
+--   normal_mode = "n",
+--   insert_mode = "i",
+--   visual_mode = "v",
+--   visual_block_mode = "x",
+--   term_mode = "t",
+--   command_mode = "c",
+
+-- Normal --
+-- Better window navigation
+keymap("n", "<C-h>", "<C-w>h", opts)
+keymap("n", "<C-j>", "<C-w>j", opts)
+keymap("n", "<C-k>", "<C-w>k", opts)
+keymap("n", "<C-l>", "<C-w>l", opts)
+
+-- Resize with arrows
+keymap("n", "<C-Up>", ":resize -2<CR>", opts)
+keymap("n", "<C-Down>", ":resize +2<CR>", opts)
+keymap("n", "<C-Left>", ":vertical resize -2<CR>", opts)
+keymap("n", "<C-Right>", ":vertical resize +2<CR>", opts)
+
+-- Navigate buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Maintain cursor position when appending lines with 'J'
+keymap("n", "J", "mzJ`z", opts)
+
+-- Keep cursor vertically centered while half-page jumping with <Ctrl-D>/<Ctrl-U>
+keymap("n", "<C-d>", "<C-d>zz", opts)
+keymap("n", "<C-u>", "<C-u>zz", opts)
+
+-- Keep cursor vertically centered when jumping through search terms
+keymap("n", "n", "nzzzv", opts)
+keymap("n", "N", "Nzzzv", opts)
+
+-- <Leader-s> Start replace command with current word under cursor
+keymap("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]], { desc = "Start replace with word under cursor", noremap = true, silent = true })
+keymap("n", "<leader>nh", ":hohl<CR>", { desc = "Clear search highlights", noremap = true, silent = true })
+
+-- Insert --
+
+-- Visual --
+-- Move blocks of text up/down in Visua mode, respecting indent
+keymap("v", "J", ":m '>+1<CR>gv=gv", opts)
+keymap("v", "K", ":m '<-2<CR>gv=gv", opts)
---
+++ nvim/.config/nvim/lua/plugins/nvim-tree.lua
@@ -0,0 +1,129 @@
+return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = false,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		local nvim_tree = require("nvim-tree")
+		--local nvim_tree_config = require("nvim-tree.config")
+		--local tree_cb = nvim_tree_config.nvim_tree_callback
+		nvim_tree.setup({
+			renderer = {
+				root_folder_label = false,
+				icons = {
+					show = {
+						file = true,
+						folder = true,
+						folder_arrow = true,
+						git = true,
+					},
+					glyphs = {
+						default = "",
+						symlink = "",
+						git = {
+							unstaged = "",
+							staged = "S",
+							unmerged = "",
+							renamed = "➜",
+							deleted = "",
+							untracked = "U",
+							ignored = "◌",
+						},
+						folder = {
+							default = "",
+							open = "",
+							empty = "",
+							empty_open = "",
+							symlink = "",
+						},
+					},
+				},
+			},
+			disable_netrw = true,
+			hijack_netrw = true,
+			open_on_tab = false,
+			hijack_cursor = false,
+			update_cwd = true,
+			hijack_directories = {
+				enable = true,
+				auto_open = true,
+			},
+			diagnostics = {
+				enable = true,
+				icons = {
+					hint = "",
+					info = "",
+					warning = "",
+					error = "",
+				},
+			},
+			update_focused_file = {
+				enable = true,
+				update_cwd = true,
+				ignore_list = {},
+			},
+			system_open = {
+				cmd = nil,
+				args = {},
+			},
+			filters = {
+				dotfiles = false,
+				custom = {},
+			},
+			git = {
+				enable = true,
+				ignore = true,
+				timeout = 500,
+			},
+			view = {
+				width = 30,
+				side = "left",
+				number = false,
+				relativenumber = false,
+			},
+			actions = {
+				use_system_clipboard = true,
+				change_dir = {
+					enable = true,
+					global = false,
+					restrict_above_cwd = false,
+				},
+				expand_all = {
+					max_folder_discovery = 300,
+				},
+				open_file = {
+					quit_on_open = false,
+					resize_window = true,
+					window_picker = {
+						enable = true,
+						chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
+						exclude = {
+							filetype = { "notify", "packer", "qf", "diff", "fugitive", "fugitiveblame" },
+							buftype = { "nofile", "terminal", "help" },
+						},
+					},
+				},
+				remove_file = {
+					close_window = true,
+				},
+			},
+			trash = {
+				cmd = "trash",
+				require_confirm = true,
+			},
+		})
+
+		local keymap = vim.keymap
+		keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })
+		keymap.set(
+			"n",
+			"<leader>ef",
+			"<cmd>NvimTreeFindFileToggle<CR>",
+			{ desc = "Toggle file explorer on current working directory" }
+		)
+		keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" })
+		keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })
+	end,
+}
